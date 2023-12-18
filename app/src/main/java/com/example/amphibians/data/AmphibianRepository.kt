c08e93788107e927ef4cff0016fd9e1db967dc82
ismat27 <adegbiteismail27@gmail.com>
2023-12-18 10:59:09 +0100

    refactor

--- app/src/main/java/com/example/amphibians/data/AmphibianRepository.kt
+++ app/src/main/java/com/example/amphibians/data/AmphibianRepository.kt
@@ -1,6 +1,5 @@
 package com.example.amphibians.data
 
-import android.util.Log
 import com.example.amphibians.network.Amphibian
 import com.example.amphibians.network.AmphibianApiService
 
@@ -11,13 +10,7 @@ interface IAmphibianRepository {
 class AmphibianRepository(private val amphibianApiService: AmphibianApiService) :
     IAmphibianRepository {
     override suspend fun getAmphibians(): List<Amphibian> {
-        try {
-            return amphibianApiService.getAmphibians()
-        } catch (e: Exception) {
-            Log.d("AmphibianRepository", e.message ?: "AmphibianRepository error")
-            return LocalDataSource.amphibians
-        }
-
+        return amphibianApiService.getAmphibians()
     }
 
 }
--- app/src/main/java/com/example/amphibians/ui/screens/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibianViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.amphibians.ui.screens
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -13,12 +14,13 @@ import com.example.amphibians.AmphibianApplication
 import com.example.amphibians.data.IAmphibianRepository
 import com.example.amphibians.network.Amphibian
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import java.io.IOException
 
 sealed interface AmphibianUiState {
     data class Success(val data: List<Amphibian>) : AmphibianUiState
-    object Error : AmphibianUiState
-    object Loading : AmphibianUiState
+    data object Error : AmphibianUiState
+    data object Loading : AmphibianUiState
 }
 
 class AmphibianViewModel(private val amphibianRepository: IAmphibianRepository) : ViewModel() {
@@ -36,7 +38,11 @@ class AmphibianViewModel(private val amphibianRepository: IAmphibianRepository) 
             amphibianUiState = try {
                 val result = amphibianRepository.getAmphibians()
                 AmphibianUiState.Success(result)
-            } catch (e: Exception) {
+            } catch (e: IOException) {
+                Log.d("AmphibianViewModel", e.message?: "error in getAmphibians")
+                AmphibianUiState.Error
+            } catch (e: HttpException) {
+                Log.d("AmphibianViewModel", e.message?: "error in getAmphibians")
                 AmphibianUiState.Error
             }
         }
